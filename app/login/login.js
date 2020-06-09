2e8a81c3aaebaa9a58a766cac864424db53542a7
Jorge Abad <jorgejoseabad@gmail.com>
2020-06-09 12:20:25 +0200

    retiro users.js y codigo muerto en loginjs

--- app/login/login.js
+++ app/login/login.js
@@ -23,14 +23,6 @@ angular.module('myApp.login', [
   //los usuarios autorizados desde el mock.
   console.log(UsersService.get());
 
-  /**
-  * datos mock para sesion storage se cargan al lanzar el controlador
-  */
-  //let usuariosAutorizados = [
-  //  {username:"usuario1",password:"password1"},
-  //  {username:"usuario2",password:"password2"},
-  //  {username:"Gengis",password:"Khan"}
-  //]
 
   $sessionStorage.valueToShare = UsersService.get();
 
